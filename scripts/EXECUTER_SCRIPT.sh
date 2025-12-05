40f5ddb11f3e563013d3efd4c9b2ac20d41c738a
Saad Rizk <saadrizk@MacBook-Pro-de-Saad.local>
2025-12-05 11:16:47 +0100

    Clean: Supprimer script inutile

--- scripts/EXECUTER_SCRIPT.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Script pour exécuter cash_forecast_analysis.py facilement
-
-cd /Users/saadrizk/Desktop/capstone
-deliverables/.venv/bin/python deliverables/Python/cash_forecast_analysis.py
-
